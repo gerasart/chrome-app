9caa8081c977f6d93da8a83ae197c1f279415805
maksalexsavchenko <makskidexom@gmail.com>
2022-06-30 13:24:51 +0300

    confirm phrase redesign

--- src/pages/CreateWallet/SeedPhrase/Confirm/ConfirmPhrase.js
+++ src/pages/CreateWallet/SeedPhrase/Confirm/ConfirmPhrase.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Header from '../../../../components/Header/Header';
-import main from '../../../../components/Main.module.scss';
+import {Link, useNavigate} from 'react-router-dom';
 import extensionStore from '../../../../store/local-store';
 import { setPhrase } from '../../../../store/slices/user';
-import confirm_phrase from './ConfirmPhrase.module.css';
+import './ConfirmPhrase.scss';
+import "../../../../assets/styles/global/_global.scss"
 
 export default function ConfirmPhrase() {
     const phrase = useSelector((state) => state.user.phrase);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const arrowBack = chrome.runtime.getURL("img/svg/arrow_back.svg");
 
     const [randomPhrase] = useState(
         phrase
@@ -56,39 +58,44 @@ export default function ConfirmPhrase() {
     };
 
     return (
-        <div className={confirm_phrase.ConfirmPhrase}>
-            <Header />
-            <div className={main.h1}>Confirm your recovery passphrase</div>
-
-            <div className={main.text}>
-                Select each phrase to make sure it is correct.
+        <div className="seed-phrase content_block">
+            <div onClick={() => navigate(-1)} className="header_back">
+                <img src={arrowBack} alt="" className="back"/>
+            </div>
+            <div className="content_text">
+                <div className="text_h1">Confirm recovery phrase</div>
+                <div className="text_p">
+                    Choose the words in the correct order
+                </div>
             </div>
 
-            <div className={confirm_phrase.phrase_container}>
+            <div className="phrase_container">
                 {confirmPhrase.map((item, index) => {
                     return (
                         <div
                             onClick={removeWord}
-                            className={confirm_phrase.phrase_word}
+                            className="phrase_container__item"
                             key={index}
                         >
-                            {item}
+                            <div className="word">
+                                {item}
+                            </div>
                         </div>
                     );
                 })}
             </div>
 
-            <div className={confirm_phrase.phrase_input}>
+            <div className="phrase_input">
                 {randomPhrase.map((item, index) => {
                     return (
                         <div
                             onClick={(e) => selectWord(e, index)}
                             className={
                                 item.disabled
-                                    ? confirm_phrase.phrase_word +
+                                    ? "phrase_word" +
                                       ' ' +
-                                      confirm_phrase.is_disabled
-                                    : confirm_phrase.phrase_word
+                                      "is_disabled"
+                                    : "phrase_word"
                             }
                             key={index}
                         >
@@ -97,15 +104,13 @@ export default function ConfirmPhrase() {
                     );
                 })}
             </div>
-            <Link to="/greetings">
-                <button
-                    onClick={seedPhraseHD}
-                    disabled={!(phrase === confirmPhrase.join(' '))}
-                    className={confirm_phrase.btn}
-                >
-                    Continue
-                </button>
-            </Link>
+            {!(phrase === confirmPhrase.join(' ')) ? (
+                <div className="btn_disabled"> Accept </div>
+            ) : (phrase === confirmPhrase.join(' ')) ? (
+                <Link to="/greetings" >
+                    <div onClick={seedPhraseHD} className="btn_main">Continue</div>
+                </Link>
+            ) : null}
         </div>
     );
 }
